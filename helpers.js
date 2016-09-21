f5a9cb77ecb6969dfe4b2ac01c7a8988e04225ca
igor.bebic@gmail.com <igor.bebic@gmail.com>
2016-09-21 15:13:16 +0200

    ES6 stuff

--- helpers.js
+++ helpers.js
@@ -11,19 +11,18 @@ function toFloat(str) {
 }
 
 function parseArgs(args) {
-  var argArray = {};
+  let argArray = {};
   args[0] = args[0].replace(',', '.');
   if (/^[0-9]+(\.)?[0-9]+$/.test(args[0])) {
     argArray[0] = args[0];
     argArray[1] = args[1];
     argArray[2] = args[3];
   } else {
-    var myArray = args[0].split(/(\d+(\.)?\d+)/);
+    let myArray = args[0].split(/(\d+(\.)?\d+)/);
     argArray[0] = myArray[1];
     argArray[1] = myArray[3];
     argArray[2] = args[2];
   }
-  // console.log(myArray);
   return argArray;
 }
 
