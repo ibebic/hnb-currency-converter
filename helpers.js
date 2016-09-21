961ad82f92fc2b0a9afd16516efa4b7d4647696d
igor.bebic@gmail.com <igor.bebic@gmail.com>
2016-09-21 14:55:15 +0200

    Bugfix for using values without decimals

--- helpers.js
+++ helpers.js
@@ -12,12 +12,13 @@ function toFloat(str) {
 
 function parseArgs(args) {
   var argArray = {};
-  if (/^[0-9]+(\.)[0-9]+$/.test(args[0].replace(',', '.'))) {
-    argArray[0] = args[0].replace(',', '.');
+  args[0] = args[0].replace(',', '.');
+  if (/^[0-9]+(\.)?[0-9]+$/.test(args[0])) {
+    argArray[0] = args[0];
     argArray[1] = args[1];
     argArray[2] = args[3];
   } else {
-    var myArray = args[0].replace(',', '.').split(/(\d+(\.)\d+)/);
+    var myArray = args[0].split(/(\d+(\.)?\d+)/);
     argArray[0] = myArray[1];
     argArray[1] = myArray[3];
     argArray[2] = args[2];
