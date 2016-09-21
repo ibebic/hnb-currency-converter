c4d7aa1b0ba737f69c2a1064b50669faf49926e0
igor.bebic@gmail.com <igor.bebic@gmail.com>
2016-09-21 13:28:26 +0200

    Added support for decimals

--- helpers.js
+++ helpers.js
@@ -12,16 +12,17 @@ function toFloat(str) {
 
 function parseArgs(args) {
   var argArray = {};
-  if (/^[0-9]+$/.test(args[0])) {
-    argArray[0] = args[0];
+  if (/^[0-9]+(\.)[0-9]+$/.test(args[0].replace(',', '.'))) {
+    argArray[0] = args[0].replace(',', '.');
     argArray[1] = args[1];
     argArray[2] = args[3];
   } else {
-    var myArray = args[0].split(/(\d+)/);
+    var myArray = args[0].replace(',', '.').split(/(\d+(\.)\d+)/);
     argArray[0] = myArray[1];
-    argArray[1] = myArray[2];
+    argArray[1] = myArray[3];
     argArray[2] = args[2];
   }
+  // console.log(myArray);
   return argArray;
 }
 
